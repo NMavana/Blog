9ea01a67f870f91219ecf8fcedaaf63db98f26bc
Mavana <mavanabrahama@gmail.com>
2026-03-04 11:33:00 +0300

    bootstrap added

---
+++ includes/function.php
@@ -0,0 +1,11 @@
+<?php
+function add_jumbotron ()
+    {
+    echo '<div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">Fluid Jumbotrom</h1>
+            <p class="lead">This is a modified humbotrom</p>
+        </div>
+    </div>';
+    }
+?>
---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+include_once "includes/function.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Dynamic Site</title>
+    <link rel="stylesheet" href="style/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <!--NAV BAR-->
+    <?php include_once "includes/nav.php"; ?>
+    <!--NAV BAR END HERE-->
+
+    <!--JUMBOTRON-->
+    <?php add_jumbotron (); ?>
+    <!--END JUMBOTRON-->
+
+    <!--CARD-->
+    <div class="container">
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Post title</h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary">Author name</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card’s content.</p>
+                <a href="#" class="btn btn-primary">Read More</a>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scroll.js"></script>
+</body>
+</html>
