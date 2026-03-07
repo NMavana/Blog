289fd359284ce8d8d98d07a58a31c7b44d740f70
Mavana <mavanabrahama@gmail.com>
2026-03-07 11:00:27 +0300

    adding profile updating functionality

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,5 @@
 <?php
+include_once ("../includes/connection.php");
 session_start ();
 
 if (isset ($_SESSION[ "author_role" ]))
@@ -86,17 +87,26 @@ if (isset ($_SESSION[ "author_role" ]))
                     </div>
 
                     <div id="admin-index-form">
+                        <?php if (isset ($_GET[ "message" ]))
+                        {
+                        $msg = $_GET[ "message" ];
+                        echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>' . $msg . '</strong>
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>';
+                        } ?>
                         <h1>Your Profile</h1>
-                        <form>
+                        <form method="post">
                             <div class="mb-3">
                                 <label for="exampleInputText" class="form-label">Name</label>
                                 <input type="text" name="author_name" class="form-control" id="exampleInputText"
-                                    placeholder="Enter Name" value="<?php echo$_SESSION[ "author_name" ]; ?>">
+                                    placeholder="Enter Name" value="<?php echo $_SESSION[ "author_name" ]; ?>">
                             </div>
                             <div class="mb-3">
                                 <label for="exampleInputEmail1" class="form-label">Email address</label>
                                 <input type="email" name="author_email" class="form-control" id="exampleInputEmail1"
-                                    aria-describedby="emailHelp" placeholder="Enter Email" value="<?php echo$_SESSION[ "author_email" ];?>">
+                                    aria-describedby="emailHelp" placeholder="Enter Email"
+                                    value="<?php echo $_SESSION[ "author_email" ]; ?>">
                             </div>
                             <div class="mb-3">
                                 <label for="exampleInputPassword1" class="form-label">Password</label>
@@ -105,11 +115,57 @@ if (isset ($_SESSION[ "author_role" ]))
                             </div>
                             <div class="mb-3">
                                 <label for="bio" class="form-label">Your Bio</label>
-                                <textarea id="story" name="author_bio" rows="5" cols="33" class="form-control">
-                                    <?php echo$_SESSION["author_bio"];?>       </textarea>
+                                <textarea id="story" name="author_bio" rows="5" cols="33"
+                                    class="form-control"><?php echo $_SESSION[ "author_bio" ]; ?></textarea>
                             </div>
-                            <button type="submit" class="btn btn-primary">Update</button>
+                            <button type="submit" class="btn btn-primary" name="update">Update</button>
                         </form>
+                        <?php
+                        if (isset ($_POST[ "update" ]))
+                            {
+                            $author_name     = mysqli_real_escape_string ( $conn, $_POST[ "author_name" ] );
+                            $author_email    = mysqli_real_escape_string ( $conn, $_POST[ "author_email" ] );
+                            $author_password = mysqli_real_escape_string ( $conn, $_POST[ "author_password" ] );
+                            $author_bio      = mysqli_real_escape_string ( $conn, $_POST[ "author_bio" ] );
+                            }
+
+                        //Checking if field are empty
+                        if (empty ($author_name) OR empty ($author_email) OR empty ($author_bio))
+                            {
+                            header("Location: index.php?message=Empty+Fields");
+                            exit();
+                            } else
+                            {
+                            // CHecking if email is valid
+                            if ( ! filter_var ( $author_email, FILTER_VALIDATE_EMAIL ))
+                                {
+                                echo "Please enter a Valid email";
+                                } else
+                                {
+                                // Check if password entered is new
+                                if (empty ($author_password))
+                                    {
+                                    // user don't want to change his password
+                                    $author_id = $_SESSION[ 'author_id' ];
+                                    $sql       = "UPDATE `author` SET `author_name`= '$author_name', `author_email`='$author_email', `author_bio`='$author_bio' WHERE `author_id`='$author_id'";
+                                    if (mysqli_query ( $conn, $sql ))
+                                        {
+                                        $_SESSION[ "author_name" ]  = $author_name;
+                                        $_SESSION[ "author_email" ] = $author_email;
+                                        $_SESSION[ "author_bio" ]   = $author_bio;
+                                        header ( "Location: index.php?message=Record+Updated" );
+                                        exit();
+                                        } else
+                                        {
+                                        echo "error";
+                                        }
+                                    } else
+                                    {
+                                    //user wants to change his password
+                                    }
+                                }
+                            }
+                        ?>
                     </div>
                 </main>
             </div>
